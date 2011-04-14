3a741af3df71f745f3213de0f8f06c8659a08fbf
Tyler Copple <J.TylerCopple@gmail.com>
2011-04-13 23:24:27 -0400

    Removing cruft generated by rails generators, also some views in favor of thier haml implementations.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,27 +1,18 @@
 class AuthorsController < ApplicationController
-  # GET /authors
   def index
     @authors = Author.order("pen_name")
   end
 
-  # GET /authors/1
   def show
     add_stylesheet('authors/show.css')
     
     @author = Author.find(params[:id])
   end
 
-  # GET /authors/1/edit
   def edit
     @author = Author.find(params[:id])
   end
 
-  # POST /authors
-  def create
-    @author = Author.new(params[:author])
-  end
-
-  # PUT /authors/1
   def update
     @author = Author.find(params[:id])
 
--- test/functional/content_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ContentControllerTest < ActionController::TestCase
-  test "should get about" do
-    get :about
-    assert_response :success
-  end
-
-end
